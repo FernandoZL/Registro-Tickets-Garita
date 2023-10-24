d4d7e3b024cceb2fc0d9449d2fb3ebe75206f97e
Fernándo ZL <18036544+FernandoZL@users.noreply.github.com>
2023-10-23 22:54:41 -0600

    Agregar archivos de proyecto.

---
+++ Registro Tickets Garita/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Registro_Tickets_Garita
+{
+    partial class FormRegistro
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRegistro));
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelApellido = new System.Windows.Forms.Label();
+            this.labelLicencia = new System.Windows.Forms.Label();
+            this.labelProveedor = new System.Windows.Forms.Label();
+            this.labelPlacas = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnVerRegistros = new System.Windows.Forms.Button();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.textBoxLicencia = new System.Windows.Forms.TextBox();
+            this.textBoxProveedor = new System.Windows.Forms.TextBox();
+            this.textBoxPlacas = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureLogo.Image")));
+            this.pictureLogo.Location = new System.Drawing.Point(195, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(172, 74);
+            this.pictureLogo.TabIndex = 0;
+            this.pictureLogo.TabStop = false;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(12, 12);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(177, 74);
+            this.labelTitulo.TabIndex = 1;
+            this.labelTitulo.Text = "Registro de unidades";
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombre.Location = new System.Drawing.Point(21, 161);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(96, 26);
+            this.labelNombre.TabIndex = 2;
+            this.labelNombre.Text = "Nombre:";
+            // 
+            // labelApellido
+            // 
+            this.labelApellido.AutoSize = true;
+            this.labelApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelApellido.Location = new System.Drawing.Point(21, 198);
+            this.labelApellido.Name = "labelApellido";
+            this.labelApellido.Size = new System.Drawing.Size(96, 26);
+            this.labelApellido.TabIndex = 3;
+            this.labelApellido.Text = "Apellido:";
+            // 
+            // labelLicencia
+            // 
+            this.labelLicencia.AutoSize = true;
+            this.labelLicencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLicencia.Location = new System.Drawing.Point(21, 237);
+            this.labelLicencia.Name = "labelLicencia";
+            this.labelLicencia.Size = new System.Drawing.Size(138, 26);
+            this.labelLicencia.TabIndex = 4;
+            this.labelLicencia.Text = "No. Licencia:";
+            // 
+            // labelProveedor
+            // 
+            this.labelProveedor.AutoSize = true;
+            this.labelProveedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProveedor.Location = new System.Drawing.Point(21, 281);
+            this.labelProveedor.Name = "labelProveedor";
+            this.labelProveedor.Size = new System.Drawing.Size(118, 26);
+            this.labelProveedor.TabIndex = 5;
+            this.labelProveedor.Text = "Proveedor:";
+            // 
+            // labelPlacas
+            // 
+            this.labelPlacas.AutoSize = true;
+            this.labelPlacas.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPlacas.Location = new System.Drawing.Point(21, 325);
+            this.labelPlacas.Name = "labelPlacas";
+            this.labelPlacas.Size = new System.Drawing.Size(84, 26);
+            this.labelPlacas.TabIndex = 6;
+            this.labelPlacas.Text = "Placas:";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(129, 415);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(116, 40);
+            this.btnRegistrar.TabIndex = 7;
+            this.btnRegistrar.Text = "Registrar ✔";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnVerRegistros
+            // 
+            this.btnVerRegistros.Location = new System.Drawing.Point(38, 79);
+            this.btnVerRegistros.Name = "btnVerRegistros";
+            this.btnVerRegistros.Size = new System.Drawing.Size(91, 23);
+            this.btnVerRegistros.TabIndex = 8;
+            this.btnVerRegistros.Text = "Ver Registros";
+            this.btnVerRegistros.UseVisualStyleBackColor = true;
+            this.btnVerRegistros.Click += new System.EventHandler(this.btnVerRegistros_Click);
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(167, 167);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(169, 20);
+            this.textBoxNombre.TabIndex = 9;
+            this.textBoxNombre.TextChanged += new System.EventHandler(this.textBoxNombre_TextChanged);
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.Location = new System.Drawing.Point(167, 204);
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(169, 20);
+            this.textBoxApellido.TabIndex = 10;
+            this.textBoxApellido.TextChanged += new System.EventHandler(this.textBoxApellido_TextChanged);
+            // 
+            // textBoxLicencia
+            // 
+            this.textBoxLicencia.Location = new System.Drawing.Point(167, 243);
+            this.textBoxLicencia.Name = "textBoxLicencia";
+            this.textBoxLicencia.Size = new System.Drawing.Size(169, 20);
+            this.textBoxLicencia.TabIndex = 11;
+            this.textBoxLicencia.TextChanged += new System.EventHandler(this.textBoxLicencia_TextChanged);
+            // 
+            // textBoxProveedor
+            // 
+            this.textBoxProveedor.Location = new System.Drawing.Point(167, 287);
+            this.textBoxProveedor.Name = "textBoxProveedor";
+            this.textBoxProveedor.Size = new System.Drawing.Size(169, 20);
+            this.textBoxProveedor.TabIndex = 12;
+            this.textBoxProveedor.TextChanged += new System.EventHandler(this.textBoxProveedor_TextChanged);
+            // 
+            // textBoxPlacas
+            // 
+            this.textBoxPlacas.Location = new System.Drawing.Point(167, 331);
+            this.textBoxPlacas.Name = "textBoxPlacas";
+            this.textBoxPlacas.Size = new System.Drawing.Size(169, 20);
+            this.textBoxPlacas.TabIndex = 13;
+            this.textBoxPlacas.TextChanged += new System.EventHandler(this.textBoxPlacas_TextChanged);
+            // 
+            // FormRegistro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 507);
+            this.Controls.Add(this.textBoxPlacas);
+            this.Controls.Add(this.textBoxProveedor);
+            this.Controls.Add(this.textBoxLicencia);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.btnVerRegistros);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.labelPlacas);
+            this.Controls.Add(this.labelProveedor);
+            this.Controls.Add(this.labelLicencia);
+            this.Controls.Add(this.labelApellido);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.pictureLogo);
+            this.Name = "FormRegistro";
+            this.Text = "Registrar Unidades";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelApellido;
+        private System.Windows.Forms.Label labelLicencia;
+        private System.Windows.Forms.Label labelProveedor;
+        private System.Windows.Forms.Label labelPlacas;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnVerRegistros;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxLicencia;
+        private System.Windows.Forms.TextBox textBoxProveedor;
+        private System.Windows.Forms.TextBox textBoxPlacas;
+    }
+}
+
---
+++ Registro Tickets Garita/Form1.cs
@@ -0,0 +1,250 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using PdfSharp.Drawing;
+using System;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows.Forms;
+using PdfSharp.Pdf;
+using System.Drawing;
+using static NPOI.HSSF.Util.HSSFColor;
+using Microsoft.Win32;
+
+namespace Registro_Tickets_Garita
+{
+    public partial class FormRegistro : Form
+    {
+        private int idIncremental = 1; // Variable para el ID incremental
+        private int numeroTurno = 1; // Variable para el número de turno
+        private DateTime ultimoInicioDelDia = DateTime.Now.Date; // Variable para el último inicio del día
+        private string registrosFilePath; // Ruta del archivo de registros
+
+
+        public FormRegistro()
+        {
+            InitializeComponent();
+            // Establecer la ruta del archivo de registros
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string registrosFolder = Path.Combine(baseDirectory, "Registros");
+            if (!Directory.Exists(registrosFolder))
+            {
+                Directory.CreateDirectory(registrosFolder);
+            }
+            registrosFilePath = Path.Combine(registrosFolder, "registros.csv");
+
+            // Cargar el último ID desde un archivo (si existe)
+            CargarUltimoID();
+            // Verificar si se inicia un nuevo día
+            if (DateTime.Now.Date != ultimoInicioDelDia)
+            {
+                numeroTurno = 1;
+                ultimoInicioDelDia = DateTime.Now.Date;
+            }
+        }
+
+
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+            // Incrementar el ID
+            int id = idIncremental++;
+
+
+            // Capturar la fecha y hora actual
+            DateTime fechaYHora = DateTime.Now;
+
+
+
+
+            // Crear una línea de registro
+            string registro = $"{id}, {numeroTurno}, {fechaYHora}, {textBoxNombre.Text}, {textBoxApellido.Text}, {textBoxLicencia.Text}, {textBoxProveedor.Text}";
+
+            // Agregar un título si el archivo no existe
+            if (!File.Exists(registrosFilePath))
+            {
+                File.WriteAllText(registrosFilePath, "ID, Turno, Fecha y Hora, Nombre, Apellido, No. Licencia, Proveedor" + Environment.NewLine);
+            }
+
+            // Escribir la línea en el archivo de registros
+            File.AppendAllText(registrosFilePath, registro + Environment.NewLine);
+
+            // Guardar el último ID
+            GuardarUltimoID();
+
+            // Imprimir el ticket
+            ImprimirTicket(id, numeroTurno, fechaYHora, textBoxNombre.Text, textBoxApellido.Text, textBoxLicencia.Text, textBoxProveedor.Text);
+
+
+            // Mostrar un mensaje de éxito o hacer otras acciones necesarias
+            MessageBox.Show("Registro completado.Turno: " + numeroTurno);
+
+            // Incrementar el número de turno
+            numeroTurno++;
+
+
+            // Reiniciar los campos de entrada si es necesario
+            LimpiarCampos();
+        }
+
+        private void CargarUltimoID()
+        {
+            string idFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ultimo_id.txt");
+
+            if (File.Exists(idFilePath) && int.TryParse(File.ReadAllText(idFilePath), out int id))
+            {
+                idIncremental = id;
+            }
+        }
+
+        private void GuardarUltimoID()
+        {
+            string idFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ultimo_id.txt");
+            File.WriteAllText(idFilePath, idIncremental.ToString());
+        }
+
+
+
+        private void LimpiarCampos()
+        {
+            // Lógica para limpiar los campos de entrada después de registrar
+            textBoxNombre.Clear();
+            textBoxApellido.Clear();
+            textBoxLicencia.Clear();
+            textBoxProveedor.Clear();
+            textBoxPlacas.Clear();
+        }
+
+
+        private void ImprimirTicket(int id, int turno, DateTime fechaYHora, string nombre, string apellido, string licencia, string proveedor)
+        {
+            // Crear un documento para imprimir
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += (sender, e) =>
+            {
+                using (System.Drawing.Font titleFont = new System.Drawing.Font("Arial", 14, FontStyle.Bold))
+                using (System.Drawing.Font normalFont = new System.Drawing.Font("Arial", 12))
+                using (Pen linePen = new Pen(Color.Black, 2)) // Pincel para la línea
+                using (SolidBrush textBrush = new SolidBrush(Color.Black)) // Pincel para el texto
+                {
+                    int x = 10; // Margen izquierdo
+                    int y = 10; // Margen superior
+
+                    // Título "Bienvenido a Suministros y Alimentos"
+                    string tituloBienvenido = "Suministros y Alimentos";
+                    e.Graphics.DrawString(tituloBienvenido, titleFont, textBrush, x, y);
+                    y += 30;
+
+                    // Título "Turno" en negrita
+                    string tituloTurno = "Turno";
+                    string valorTurno = turno.ToString();
+                    e.Graphics.DrawString(tituloTurno, titleFont, textBrush, x, y);
+                    e.Graphics.DrawString(valorTurno, titleFont, textBrush, x + 100, y); // Ajusta la posición del turno
+                    y += 40;
+
+                    // Línea horizontal (ajustada al ancho del papel)
+                    int lineaLongitud = 250; // Ancho de la línea
+                    e.Graphics.DrawLine(linePen, x, y, x + lineaLongitud, y);
+                    y += 20;
+
+                    // Otros detalles (ajustados al ancho del papel)
+                    string tituloFecha = "Fecha y Hora:";
+                    string valorFecha = fechaYHora.ToString();
+                    e.Graphics.DrawString(tituloFecha, titleFont, textBrush, x, y);
+                    e.Graphics.DrawString(valorFecha, normalFont, textBrush, x, y + titleFont.GetHeight());
+                    y += 40;
+
+                    string tituloNombre = "Nombre:";
+                    string valorNombre = nombre;
+                    e.Graphics.DrawString(tituloNombre, titleFont, textBrush, x, y);
+                    e.Graphics.DrawString(valorNombre, normalFont, textBrush, x, y + titleFont.GetHeight());
+                    y += 40;
+
+                    string tituloApellido = "Apellido:";
+                    string valorApellido = apellido;
+                    e.Graphics.DrawString(tituloApellido, titleFont, textBrush, x, y);
+                    e.Graphics.DrawString(valorApellido, normalFont, textBrush, x, y + titleFont.GetHeight());
+                    y += 40;
+
+                    // ... continúa con otros detalles
+
+                    string tituloLicencia = "No. Licencia:";
+                    string valorLicencia = licencia;
+                    e.Graphics.DrawString(tituloLicencia, titleFont, textBrush, x, y);
+                    e.Graphics.DrawString(valorLicencia, normalFont, textBrush, x, y + titleFont.GetHeight());
+                    y += 40;
+
+                    string tituloProveedor = "Proveedor:";
+                    string valorProveedor = proveedor;
+                    e.Graphics.DrawString(tituloProveedor, titleFont, textBrush, x, y);
+                    e.Graphics.DrawString(valorProveedor, normalFont, textBrush, x, y + titleFont.GetHeight());
+                    y += 40;
+
+                    // string numeroID = $"Registro: {id}";
+                    string titulonumeroID = "Registro:";
+                    string numeroID = $" {id}";
+                    e.Graphics.DrawString(titulonumeroID, titleFont, textBrush, x, y);
+                    e.Graphics.DrawString(numeroID, normalFont, textBrush, x, y + titleFont.GetHeight());
+                    y += 10;
+
+                    // Línea horizontal (ajustada al ancho del papel)
+                    e.Graphics.DrawLine(linePen, x, y + 40, x + lineaLongitud, y + 40);
+
+                    // Agradecimiento y número de ID
+                    string agradecimiento = "¡Gracias por visitarnos!";
+                    
+
+                    y += 40;
+                   
+                    e.Graphics.DrawString(agradecimiento, titleFont, textBrush, x, y);
+                   
+
+                   
+                }
+            };
+
+            // Configurar la impresora (opcional)
+            PrintDialog printDialog = new PrintDialog();
+            printDocument.PrinterSettings = printDialog.PrinterSettings;
+
+            // Imprimir el documento
+            printDocument.Print();
+        }
+
+
+
+        private void textBoxNombre_TextChanged(object sender, EventArgs e)
+        {
+            // Código que deseas ejecutar cuando cambia el texto en el control textBoxNombre.
+        }
+
+        private void textBoxApellido_TextChanged(object sender, EventArgs e)
+        {
+            // Código que deseas ejecutar cuando cambia el texto en el control textBoxApellido.
+        }
+
+        private void textBoxLicencia_TextChanged(object sender, EventArgs e)
+        {
+            // Código que deseas ejecutar cuando cambia el texto en el control textBoxLicencia.
+        }
+        private void textBoxProveedor_TextChanged(object sender, EventArgs e)
+        {
+            // Código que deseas ejecutar cuando cambia el texto en el control textBoxProveedor.
+        }
+
+        private void textBoxPlacas_TextChanged(object sender, EventArgs e)
+        {
+            // Código que deseas ejecutar cuando cambia el texto en el control textBoxPlacas.
+        }
+
+        private void btnVerRegistros_Click(object sender, EventArgs e)
+        {
+            FormVerRegistros form = new FormVerRegistros();
+            form.Show();
+        }
+
+
+
+
+    }
+}
